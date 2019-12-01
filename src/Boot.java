5e1001a4501d7b6a07e614631d618b5650ad164f
LonleyBitch <804710107@qq.com>
2019-12-01 17:01:41 +0800

    2号修改 二号修改 第二次

--- src/Boot.java
+++ src/Boot.java
@@ -3,6 +3,7 @@ public class Boot {
     public static void main(String[] args) {
         System.out.println("hello");
 
+        System.out.println("hellpp");
         System.out.println("tody");
     }
 }
